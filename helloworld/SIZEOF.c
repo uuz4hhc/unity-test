02d396b9ebc922aa39cc178f2c254a271926ab23
uuz4hhc <196400987+uuz4hhc@users.noreply.github.com>
2025-11-07 20:55:46 +0800

    清除文件

--- helloworld/SIZEOF.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("long int: %d.\n", sizeof(float));
-    return 0;
-    }
--- helloworld/char.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()  {
-  char ch = 'a';
-  printf("%c\n" , ch);
-  return 0;
-  }
